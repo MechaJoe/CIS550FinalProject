f7db1c71c48063833b80f39e9389b7c420a82d60
Joe Konno <jkonno@seas.upenn.edu>
2022-04-08 15:52:48 -0400

    Begin working on Search, server route skeleton setup

---
+++ server/router.js
@@ -0,0 +1,27 @@
+const express = require('express')
+
+const router = express.Router()
+
+router.get('/search', async (req, res) => {
+  // TODO: Query the DB based on the query params
+  res.send('TODO')
+})
+
+router.get('/user', async (req, res) => {
+  // TODO: Query the DB for the current user (taken from req.session)
+})
+
+router.post('/user', async (req, res) => {
+  // TODO: Update the DB for the current user
+})
+
+router.post('/logout', async (req, res) => {
+  req.session.username = null
+  res.redirect('/login')
+})
+
+router.post('/login', async (req, res, next) => {
+  // TODO: Login the user (using PassportJS)
+})
+
+module.exports = router
