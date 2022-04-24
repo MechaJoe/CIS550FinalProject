df5fe8f66cc34774992893873adba21213c5b538
Jason Hom <61599018+homjason@users.noreply.github.com>
2022-04-24 01:31:31 -0400

    Search Routes for Song and Artist Done

--- server/router.js
+++ server/router.js
@@ -35,7 +35,11 @@ router.post('/login', async (req, res, next) => {
   // TODO: Login the user (using PassportJS)
 })
 
-router.get('/user/top_artists', async (req, res) => {
+/*
+  Determines the top artists for a user relative to the songs that they have liked a
+  nd displays the percentage of their liked songs for that artist - COMPLEX
+*/
+router.get('/user/top-artists', async (req, res) => {
   const { username } = req.session.username
   connection.query(`
   WITH userSongs AS (
@@ -60,6 +64,90 @@ router.get('/user/top_artists', async (req, res) => {
     )
     SELECT ap.listening_percentage AS listening_percentage, a.name AS name, a.location AS location
     FROM artistPercentages ap JOIN Artist a ON ap.artist_id = a.artist_id
+  `, (error, results) => {
+    if (error) {
+      res.json({ error })
+    } else if (results) {
+      res.json({ results })
+    }
+  })
+})
+
+// For the home page, select random songs from database for users to peruse
+router.get('/get-random-songs', async (req, res) => {
+  connection.query(`
+  SELECT *
+  FROM Song
+  ORDER BY RAND()
+  LIMIT 20
+  `, (error, results) => {
+    if (error) {
+      res.json({ error })
+    } else if (results) {
+      res.json({ results })
+    }
+  })
+})
+
+router.get('/search/song', async (req, res) => {
+  const song = req.query.song ? req.query.song : ''
+  const acousticnessLow = req.query.acousticnessLow ? req.query.acousticnessLow : 0
+  const acousticnessHigh = req.query.acousticnessHigh ? req.query.acousticnessHigh : 1
+  const danceabilityLow = req.query.danceabilityLow ? req.query.danceabilityLow : 0
+  const danceabilityHigh = req.query.danceabilityHigh ? req.query.danceabilityHigh : 1
+  const energyLow = req.query.energyLow ? req.query.energyLow : 0
+  const energyHigh = req.query.energyHigh ? req.query.energyHigh : 1
+  const valenceLow = req.query.valenceLow ? req.query.valenceLow : 0
+  const valenceHigh = req.query.valenceHigh ? req.query.valenceHigh : 1
+  const livenessLow = req.query.livenessLow ? req.query.livenessLow : 0
+  const livenessHigh = req.query.livenessHigh ? req.query.livenessHigh : 1
+  const speechinessLow = req.query.speechinessLow ? req.query.speechinessLow : 0
+  const speechinessHigh = req.query.speechinessHigh ? req.query.speechinessHigh : 1
+  connection.query(`
+  SELECT *
+  FROM Song JOIN ComposedBy cb ON Song.song_id = cb.song_id
+  WHERE artist LIKE '%${song}%' AND acousticness >= ${acousticnessLow} AND 
+  acousticness <= ${acousticnessHigh} AND danceability >= ${danceabilityLow} AND 
+  danceability <= ${danceabilityHigh} AND energy >= ${energyLow} AND 
+  energy <= ${energyHigh} AND valence >= ${valenceLow} AND valence <= ${valenceHigh}AND 
+  liveness >= ${livenessLow} AND liveness <= ${livenessHigh} AND 
+  speechiness >= ${speechinessLow} ANDspeechiness <= ${speechinessHigh}
+  `, (error, results) => {
+    if (error) {
+      res.json({ error })
+    } else if (results) {
+      res.json({ results })
+    }
+  })
+})
+
+router.get('/search/artist', async (req, res) => {
+  const artist = req.query.artist ? req.query.artist : ''
+  const acousticnessLow = req.query.acousticnessLow ? req.query.acousticnessLow : 0
+  const acousticnessHigh = req.query.acousticnessHigh ? req.query.acousticnessHigh : 1
+  const danceabilityLow = req.query.danceabilityLow ? req.query.danceabilityLow : 0
+  const danceabilityHigh = req.query.danceabilityHigh ? req.query.danceabilityHigh : 1
+  const energyLow = req.query.energyLow ? req.query.energyLow : 0
+  const energyHigh = req.query.energyHigh ? req.query.energyHigh : 1
+  const valenceLow = req.query.valenceLow ? req.query.valenceLow : 0
+  const valenceHigh = req.query.valenceHigh ? req.query.valenceHigh : 1
+  const livenessLow = req.query.livenessLow ? req.query.livenessLow : 0
+  const livenessHigh = req.query.livenessHigh ? req.query.livenessHigh : 1
+  const speechinessLow = req.query.speechinessLow ? req.query.speechinessLow : 0
+  const speechinessHigh = req.query.speechinessHigh ? req.query.speechinessHigh : 1
+  connection.query(`
+  WITH artistSongs AS (
+    SELECT *
+    FROM Artist a JOIN ComposedBy cb ON a.artist_id = cb.artist_id
+  )
+  SELECT *
+  FROM Song s JOIN artistSongs artSongs ON s.song_id = artSongs.song_id
+  WHERE artSongs.name LIKE '%${artist}%' AND acousticness >= ${acousticnessLow} AND 
+  acousticness <= ${acousticnessHigh} AND danceability >= ${danceabilityLow} AND 
+  danceability <= ${danceabilityHigh} AND energy >= ${energyLow} AND energy <= ${energyHigh} AND 
+  valence >= ${valenceLow} AND valence <= ${valenceHigh} AND liveness >= ${livenessLow} AND 
+  liveness <= ${livenessHigh} AND speechiness >= ${speechinessLow} AND 
+  speechiness <= ${speechinessHigh}
   `, (error, results) => {
     if (error) {
       res.json({ error })
