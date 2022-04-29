2255def9bfbcd10d122defe7141e65fbfdd8082d
Jason Hom <61599018+homjason@users.noreply.github.com>
2022-04-29 15:36:20 -0400

    Dropdown for search/artist

--- client/src/pages/SearchPage.js
+++ client/src/pages/SearchPage.js
@@ -1,21 +1,43 @@
-import React from 'react'
-import Box from '@mui/material/Box'
-import TextField from '@mui/material/TextField'
-import Typography from '@mui/material/Typography'
+import React, { useState } from 'react'
+import FormControl from '@mui/material/FormControl'
+import InputLabel from '@mui/material/InputLabel'
+import Select from '@mui/material/Select'
+import MenuItem from '@mui/material/MenuItem'
 
-export default function SearchPage() {
+function SearchPage() {
+  const [searchType, setSearchType] = useState('')
   return (
-    <Box
-      sx={{
-        width: '80%',
-        justify: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Typography variant="h1" component="h2">
-        Music Bar!
-      </Typography>
-      <TextField fullWidth label="song or artist name..." id="search" />
-    </Box>
+    <FormControl fullWidth>
+      <InputLabel id="search-type-label">Search By</InputLabel>
+      <Select
+        labelId="search-type-label"
+        id="demo-simple-select"
+        value={searchType}
+        label="Search By"
+        onChange={(e) => setSearchType(e.target.value)}
+      >
+        <MenuItem value="song">Song</MenuItem>
+        <MenuItem value="artist">Artist</MenuItem>
+      </Select>
+    </FormControl>
   )
 }
+
+export default SearchPage
+
+// export default function SearchPage() {
+//   return (
+//     <Box
+//       sx={{
+//         width: '80%',
+//         justify: 'center',
+//         alignItems: 'center',
+//       }}
+//     >
+//       <Typography variant="h1" component="h2">
+//         Music Bar!
+//       </Typography>
+//       <TextField fullWidth label="song or artist name..." id="search" />
+//     </Box>
+//   )
+// }
