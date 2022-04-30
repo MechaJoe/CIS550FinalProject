799b28296640a72e93afd6721a5b90383ed61a26
Amy-Z <amyzhou7@wharton.upenn.edu>
2022-04-30 11:57:27 -0400

    Updated fetcher with song routes

--- client/src/fetcher.js
+++ client/src/fetcher.js
@@ -22,8 +22,8 @@ const getMatch = async (id) => {
     return res.json()
 }
 
-const getPlayer = async (id) => {
-    var res = await fetch(`http://${config.server_host}:${config.server_port}/player?id=${id}`, {
+const getSong = async (id) => {
+    var res = await fetch(`http://${config.server_host}:${config.server_port}/song/song_info?id=${id}`, {
         method: 'GET',
     })
     return res.json()
@@ -37,29 +37,17 @@ const getMatchSearch = async (home, away, page, pagesize) => {
 }
 
 const getSongSearch = async (title, artist, acousticness, danceability, duration_ms, energy, explicit, instrumentalness, liveness, loudness, mode, popularity, speechiness, tempo, valence, year, page, pagesize) => {
-    var res = await fetch(`http://${config.server_host}:${config.server_port}/search/songs?title=${title}&artist=${artist}&acousticness=${acousticness}&danceability=${danceability}&duration_ms=${duration_ms}&energy=${energy}&explicit=${explicit}&instrumentalness=${instrumentalness}&liveness=${liveness}&loudness=${loudness}&mode=${mode}&popularity=${popularity}&speechiness=${speechiness}&tempo=${tempo}&valence=${valence}&year=${year}&page=${page}&pagesize=${pagesize}`, {
+    var res = await fetch(`http://${config.server_host}:${config.server_port}/search/song?title=${title}&artist=${artist}&acousticness=${acousticness}&danceability=${danceability}&duration_ms=${duration_ms}&energy=${energy}&explicit=${explicit}&instrumentalness=${instrumentalness}&liveness=${liveness}&loudness=${loudness}&mode=${mode}&popularity=${popularity}&speechiness=${speechiness}&tempo=${tempo}&valence=${valence}&year=${year}&page=${page}&pagesize=${pagesize}`, {
         method: 'GET',
     })
     return res.json()
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
 export {
     getAllMatches,
     getAllPlayers,
     getMatch,
-    getPlayer,
+    getSong,
     getMatchSearch,
     getSongSearch
 }
