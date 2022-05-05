a68ddf66e42698af6641386cc32e2571bb6509de
audrey-yang <ayang24.robin@gmail.com>
2022-05-05 16:59:02 -0400

    Refactor Artist with MUI

--- client/src/pages/ArtistPage.js
+++ client/src/pages/ArtistPage.js
@@ -1,105 +1,83 @@
-/* eslint-disable */
-import React from 'react';
-import { Form, FormInput, FormGroup, Button, Card, CardBody, Progress } from 'shards-react';
+import React, { useState, useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
 
-import { Table, Row, Col, Divider, Slider, Rate } from 'antd'
-import { format } from 'd3-format'; 
-
+import Grid from '@mui/material/Grid'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import Divider from '@mui/material/Divider'
+import Typography from '@mui/material/Typography'
+import Box from '@mui/material/Box'
 
 import NavBar from '../components/NavBar'
-import { getArtistSearch, getArtist } from '../fetcher'
+import { getArtist } from '../fetcher'
 
-import { color, fontSize } from '@mui/system';
-const wideFormat = format('.3r');
+export default function ArtistPage() {
+  const location = useLocation()
+  const selectedArtistId = location.search.substring(4)
+  const [selectedArtistDetails, setSelectedArtistDetails] = useState(null)
 
-class ArtistPage extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            selectedArtistId: window.location.search ? window.location.search.substring(1).split('=')[1] : 229594,
-            selectedArtistDetails: null,
-            artistResults: []
-        }
-    }
+  useEffect(() => {
+    getArtist(selectedArtistId)
+      .then((res) => {
+        setSelectedArtistDetails(res.results[0])
+      })
+  }, [location, selectedArtistId])
 
-    componentDidMount() {
-        getArtistSearch(null, null).then(res => {
-            this.setState({ artistResults: res.results })
-        })
+  return (
+    <div>
+      <NavBar />
+      <Divider />
+      {selectedArtistDetails ? (
+        <Box sx={{ width: '70vw', margin: '0 auto', marginTop: '2vh' }}>
+          <Card sx={{ margin: '2rem' }}>
+            <CardContent>
+              <Grid container direction="row" justifyContent="center" alignItems="center">
+                <Grid container direction="column">
+                  <Typography variant="h3">{selectedArtistDetails?.name?.toUpperCase()}</Typography>
+                </Grid>
+              </Grid>
+              <Grid container direction="row" justifyContent="left" alignItems="center">
+                <Typography variant="h5">{selectedArtistDetails?.location}</Typography>
+                <Typography variant="h5" sx={{ padding: '0 1rem' }}>•</Typography>
+                <Typography variant="h5">{selectedArtistDetails?.genre}</Typography>
+              </Grid>
+              <Grid container direction="row" justifyContent="left" alignItems="center">
+                <Grid container direction="column">
+                  <Typography>
+                    {`Number of Listeners: ${selectedArtistDetails?.listeners}`}
+                  </Typography>
+                </Grid>
+              </Grid>
+              <Grid container direction="row" justifyContent="left" alignItems="center">
+                <Grid container direction="column">
+                  <Typography>
+                    {`Number of Scrobbles: ${selectedArtistDetails?.scrobbles}`}
+                  </Typography>
+                </Grid>
+              </Grid>
+            </CardContent>
+          </Card>
 
-        getArtist(this.state.selectedArtistId).then(res => {
-            this.setState({ selectedArtistDetails: res.results[0] })
-        })
+          <Card sx={{ margin: '2rem' }}>
+            <CardContent>
+              <Grid container direction="row" justifyContent="left" alignItems="center">
+                <Grid container direction="column">
+                  <Typography variant="h5">
+                    {`Related tags to ${selectedArtistDetails?.name?.toUpperCase()}`}
+                  </Typography>
+                </Grid>
+              </Grid>
+              <Grid container direction="row" justifyContent="left" alignItems="center">
+                <Grid container direction="column">
+                  <Typography>{selectedArtistDetails?.tags}</Typography>
+                </Grid>
+              </Grid>
+            </CardContent>
+          </Card>
 
-    }
+        </Box>
+      ) : null}
 
-    render() {
-        return (
-
-            <div>
-
-                <NavBar />
-
-                <Divider />
-                {this.state.selectedArtistDetails ? <div style={{ width: '70vw', margin: '0 auto', marginTop: '2vh' }}>
-                    <Card>
-                    
-                        <CardBody>
-                            <Row gutter='30' align='middle' justify='center'>
-                                <Col flex={2} style={{ textAlign: 'left' }}>
-                                    <h3>{this.state.selectedArtistDetails.name}</h3>
-                                </Col>
-                            </Row>
-                            
-                            <Row gutter='30' align='middle' justify='left'>
-                                <Col>
-                                <h5>{this.state.selectedArtistDetails.location}</h5>
-                                </Col>
-                                <Col>
-                                <h5>{this.state.selectedArtistDetails.genre}</h5>
-                                </Col>
-                            </Row>
-
-                            <Row gutter='30' align='middle' justify='left'>
-                                <Col>
-                                # of Listeners: {this.state.selectedArtistDetails.listeners}
-                                </Col>
-                            </Row>
-
-                            <Row gutter='30' align='middle' justify='left'>
-                                <Col>
-                                # of Scrobbles: {this.state.selectedArtistDetails.scrobbles}
-                                </Col>
-                            </Row>
-
-                        </CardBody>
-
-                    </Card>
-
-                    <br/>
-                    <br/>
-
-                    <Card>
-                        <CardBody>
-                            <Row gutter='30' align='middle' justify='center'>
-                                <Col flex={2} style={{ textAlign: 'left' }}>
-                                    <h5>related tags to {this.state.selectedArtistDetails.name}</h5>
-                                </Col>
-                            </Row>
-                            <Row gutter='30' align='middle' justify='left'>
-                                <Col>
-                                    {this.state.selectedArtistDetails.tags}
-                                </Col>
-                            </Row>
-                        </CardBody>
-                    </Card>
-
-                </div> : null}
-
-            </div>
-        )
-    }
+    </div>
+  )
 }
-
-export default ArtistPage
-
--- server/router.js
+++ server/router.js
@@ -1,10 +1,31 @@
 const express = require('express')
 const mysql = require('mysql')
+// const config = require('./config.json')
+
+// const router = express.Router()
+
+// const connection = mysql.createConnection({
+//   host: process.env.RDS_HOST ? process.env.RDS_HOST : config.rds_host,
+//   user: process.env.RDS_USER ? process.env.RDS_USER : config.rds_user,
+//   password: process.env.RDS_PASSWORD ? process.env.RDS_PASSWORD : config.rds_password,
+//   port: process.env.RDS_PORT ? process.env.RDS_PORT : config.rds_port,
+//   database: process.env.RDS_DB ? process.env.RDS_DB : config.rds_db,
+// })
+
+// connection.connect()
+
+// TEMP FIX FOR SESSION PROBLEM
+let session
+
+// const mysql = require('mysql')
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oidc')
+const LinkedInStrategy = require('passport-linkedin-oauth2').Strategy
+const crypto = require('node:crypto')
 const config = require('./config.json')
 
-const router = express.Router()
-
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
+  connectionLimit: 10,
   host: process.env.RDS_HOST ? process.env.RDS_HOST : config.rds_host,
   user: process.env.RDS_USER ? process.env.RDS_USER : config.rds_user,
   password: process.env.RDS_PASSWORD ? process.env.RDS_PASSWORD : config.rds_password,
@@ -12,11 +33,185 @@ const connection = mysql.createConnection({
   database: process.env.RDS_DB ? process.env.RDS_DB : config.rds_db,
 })
 
-connection.connect()
+const router = express.Router()
 
-// TEMP FIX FOR SESSION PROBLEM
-let session
+router.post('/login', async (req, res) => {
+  const { body } = req
+  const { username, password } = body
+  const sql = `SELECT password
+  FROM User u
+  WHERE u.username = '${username}'`
+  connection.query(sql, (error, results) => {
+    if (error) {
+      res.json({ error })
+    } else if (results) {
+      if (results.length !== 0 && results[0].password === password) {
+        req.session.username = username
+        req.session.save()
+        session = req.session
+        // console.log(req.session)
+        res.send('Successful login')
+      } else {
+        res.send('Unsuccessful login')
+      }
+    }
+  })
+})
 
+router.post('/signup', (req, res) => {
+  const {
+    username, password, first_name, last_name, pronouns, location,
+  } = req.body
+  console.log(req.body)
+  const sql = `INSERT INTO User (username, password, first_name, last_name, pronouns, location)
+               VALUES ('${username}', '${password}', '${first_name}', '${last_name}', '${pronouns}', '${location}')`
+  connection.query(sql, (error, results) => {
+    if (error) {
+      res.json({ error })
+    } else if (results) {
+      req.session.username = username
+      res.send('Successful signup')
+    }
+  })
+})
+
+router.post('/federated-signup', (req, res) => {
+  const {
+    first_name, last_name, pronouns, location,
+  } = req.body
+  const { username } = session
+  console.log(req.session)
+  const password = crypto.pbkdf2Sync(username, 'joeisunhackable', 100000, 64, 'sha512').toString('hex')
+  const sql = `INSERT INTO User (username, password, first_name, last_name, pronouns, location)
+               VALUES ('${username}', '${password}', '${first_name}', '${last_name}', '${pronouns}', '${location}')`
+  connection.query(sql, (error, results) => {
+    if (error) {
+      res.json({ error })
+    } else if (results) {
+      req.session.username = username
+      res.send('Successful federated signup')
+    }
+  })
+})
+
+router.get('/username', (req, res) => {
+  if (req.session.passport) {
+    req.session.username = session.passport.user.id
+  }
+  res.json(req.session.username)
+})
+
+router.post('/logout', (req, res) => {
+  req.logout()
+  req.session.username = null
+  res.send('Logged out')
+})
+
+const verify = async (issuer, profile, cb) => {
+  const username = profile.id
+  connection.query(
+    `SELECT * FROM User WHERE username = '${username}'`,
+    (error, results) => {
+      if (error || !results || results.length === 0) {
+        const newProfile = profile
+        console.log(profile)
+        newProfile.create = true
+        return cb(null, newProfile)
+      }
+      return cb(null, profile)
+    },
+  )
+}
+
+router.get('/login/federated/linkedin', passport.authenticate('linkedin'))
+
+passport.use(new LinkedInStrategy(
+  {
+    clientID: process.env.LINKEDIN_CLIENT_ID ? process.env.LINKEDIN_CLIENT_ID : config.linkedin_client_id,
+    clientSecret: process.env.LINKEDIN_CLIENT_SECRET ? process.env.LINKEDIN_CLIENT_SECRET : config.linkedin_client_secret,
+    callbackURL: '/oauth2/redirect/linkedin',
+    scope: ['r_emailaddress', 'r_liteprofile'],
+    state: true,
+  },
+  ((accessToken, refreshToken, profile, cb) => {
+    console.log(profile)
+    const username = profile.id
+    connection.query(
+      `SELECT * FROM User WHERE username = '${username}'`,
+      (error, results) => {
+        if (error || !results || results.length === 0) {
+          const newProfile = profile
+          console.log(profile)
+          newProfile.create = true
+          return cb(null, newProfile)
+        }
+        return cb(null, profile)
+      },
+    )
+  }),
+))
+
+router.get(
+  '/oauth2/redirect/linkedin',
+  (req, res, next) => {
+    passport.authenticate(
+      'linkedin',
+      (err, user) => {
+        if (user && user.create) {
+          req.session.username = user.id
+          return res.redirect('http://localhost:3000/federated-signup')
+        }
+        if (user) {
+          req.session.username = user.id
+          return res.redirect('http://localhost:3000/')
+        }
+        return res.redirect('http://localhost:3000/login')
+      },
+    )(req, res, next)
+  },
+)
+
+router.get('/login/federated/google', passport.authenticate('google'))
+
+passport.use(new GoogleStrategy(
+  {
+    clientID: process.env.GOOGLE_CLIENT_ID ? process.env.GOOGLE_CLIENT_ID : config.google_client_id,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET ? process.env.GOOGLE_CLIENT_SECRET : config.google_client_secret,
+    callbackURL: '/oauth2/redirect/google',
+    scope: ['profile'],
+  },
+  verify,
+))
+
+router.get(
+  '/oauth2/redirect/google',
+  (req, res, next) => {
+    passport.authenticate(
+      'google',
+      (err, user) => {
+        if (user && user.create) {
+          req.session.username = user.id
+          return res.redirect('http://localhost:3000/federated-signup')
+        }
+        if (user) {
+          req.session.username = user.id
+          return res.redirect('http://localhost:3000/')
+        }
+        return res.redirect('http://localhost:3000/login')
+      },
+    )(req, res, next)
+  },
+)
+
+passport.serializeUser((user, cb) => {
+  process.nextTick(() => {
+    cb(null, { username: user.id })
+  })
+})
+
+passport.deserializeUser((user, cb) => {
+  process.nextTick(() => cb(null, user))
+})
 
 // AUTH ROUTES
 // router.post('/login', async (req, res) => {
@@ -34,8 +229,7 @@ let session
 //       if (results.length !== 0 && results[0].password === password) {
 //         req.
 
-
-.username = username
+// .username = username
 //         req.session.save()
 //         session = req.session
 //         // console.log(req.session)
@@ -323,7 +517,7 @@ router.get('/song/by-related-all-attributes', async (req, res) => {
 
 // Song: Recommend songs that were liked by other users in the same geographic location
 router.get('/song/recommended-by-location', async (req, res) => {
-  const { location } = req.session
+  const { location } = session
   connection.query(`
       WITH song_location_likes AS (
         SELECT location, song_id, COUNT(*) AS num_likes
@@ -362,7 +556,7 @@ router.get('/artist/info', async (req, res) => {
 
 // Artist: Match user to artist based on average attribute values
 router.get('/artist/recommended-by-attrs', async (req, res) => {
-  const { username } = req.session
+  const { username } = session
   connection.query(`
       WITH user_agg_song_attrs AS (
         SELECT 
@@ -524,7 +718,7 @@ router.get('/artist/songs-most-liked', async (req, res) => {
 
 // Artist: Recommend artists that were liked by other users in the same geographic location - COMPLEX
 router.get('/artist/recommended-by-location', async (req, res) => {
-  const { location } = req.session
+  const { location } = session
   connection.query(`
     WITH song_location_likes AS (
       SELECT location, song_id, COUNT(*) AS num_likes
